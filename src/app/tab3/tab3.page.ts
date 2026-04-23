0d1f858bc311e6d36f9d85900b03f0f34db203f8
yuyetingxiao <2939708850@qq.com>
2026-04-23 20:27:40 +0800

    Complete the logical writing of the privacy and security requirements page

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,12 @@
-import { Category, Inventory, StockStatus } from '../app.component';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
 
-import { Component } from '@angular/core';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 @Component({
   selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  templateUrl: './tab3.page.html',
+  styleUrls: ['./tab3.page.scss'],
 })
-export class Tab3Page {
-  constructor() {}
+export class Tab3Page implements OnInit {
+  constructor() { }
+
+  ngOnInit() {}
 }
